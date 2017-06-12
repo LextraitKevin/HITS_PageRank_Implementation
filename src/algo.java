10022176b71f41a8221e67d0a108f79e390e2be4
Kevin Lextrait <kevinlextrait@gmail.com>
2017-06-12 09:27:13 +0200

    initial commit

---
+++ src/algo.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * Created by kevin on 29/05/2017.
+ */
+public class algo {
+
+    public Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+
+
+    public void HITS(Map graph){
+
+        for(int i=0; i < graph.size(); i++){
+
+            Object variable = graph.get(i);
+
+
+        }
+
+
+    }
+
+
+
+
+    public static void main(String [] args){
+
+    }
+}
+
+
+
