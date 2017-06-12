1006d6e0eb14b49329ff94c914379fb617494665
Kevin Lextrait <kevinlextrait@gmail.com>
2017-06-12 10:08:09 +0200

    Add fonction read file

--- src/DataFile.java
+++ src/DataFile.java
@@ -8,8 +8,33 @@
 public class DataFile {
 
 	private String Type;
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public Data getParam() {
+		return param;
+	}
+
+	public void setParam(Data param) {
+		this.param = param;
+	}
+
 	private Data param;
 
+
+	@Override
+	public String toString() {
+		return "DataFile{" +
+				"Type='" + Type + '\'' +
+				", param=" + param.first + " " + param.second+
+				'}';
+	}
 
 	public DataFile(String type, Integer firstValue, String secondValue){
 		this.Type= type;
--- src/algo.java
+++ src/algo.java
@@ -1,16 +1,62 @@
 import java.lang.reflect.Array;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by kevin on 29/05/2017.
  */
 public class algo {
 
-    public List dataFiles = new ArrayList();
+    private static final String FILENAME = "gr0.California.txt";
 
-    private void readfile(){
+    public static List dataFiles = new ArrayList();
 
-        
+    private static void readfile(){
+
+        BufferedReader br=null;
+        FileReader fr=null;
+
+        try {
+
+            fr = new FileReader(FILENAME);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+
+            br = new BufferedReader(new FileReader(FILENAME));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+
+                String[] tab = sCurrentLine.split("\\s+");
+                DataFile data = new DataFile(tab[0],Integer.parseInt(tab[1]),tab[2]);
+
+                dataFiles.add(data);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+
     }
 
 
@@ -28,9 +74,10 @@ public class algo {
     }
 
 
-
-
     public static void main(String [] args){
+
+        readfile();
+
 
     }
 }
