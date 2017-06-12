6decb07fca940334c7d867817b269fa9ce69f962
Kevin Lextrait <kevinlextrait@gmail.com>
2017-06-12 15:54:30 +0200

    get child and parent node

--- src/algo.java
+++ src/algo.java
@@ -9,9 +9,12 @@ import java.io.IOException;
  */
 public class algo {
 
-    private static final String FILENAME = "gr0.California.txt";
+    private static final String FILENAME = "gr0.California2.txt";
 
     public static List dataFiles = new ArrayList();
+
+
+    public static Map<Integer,ArrayList> dataset = new HashMap<>();
 
     private static void readfile(){
 
@@ -77,6 +80,68 @@ public class algo {
     public static void main(String [] args){
 
         readfile();
+
+
+
+        for(int i=0; i<dataFiles.size();i++){
+
+            DataFile data = (DataFile) dataFiles.get(i);
+
+            //On calcule enfants et parents
+            if(data.getType().equals("e")){
+
+                ArrayList oldValueschild = dataset.get(data.getParam().first);
+                ArrayList oldValuesparent = dataset.get(data.getParam().second);
+
+
+                if( oldValueschild==null){
+
+                    System.out.println("null");
+                    oldValueschild = new ArrayList();
+                    List<Integer> enfants = new ArrayList<>();
+                    List<Integer> parents = new ArrayList<>();
+
+                    oldValueschild.add(enfants);
+                    oldValueschild.add(parents);
+
+                }
+                if( oldValuesparent==null){
+
+                    System.out.println("null");
+                    oldValuesparent = new ArrayList();
+                    List<Integer> enfants = new ArrayList<>();
+                    List<Integer> parents = new ArrayList<>();
+                    oldValuesparent.add(enfants);
+                    oldValuesparent.add(parents);
+
+                }
+
+                List<Integer> enfants1 = (List<Integer>) oldValueschild.get(0);
+                List<Integer> parents1 = (List<Integer>) oldValueschild.get(1);
+
+                List<Integer> enfants2 = (List<Integer>) oldValuesparent.get(0);
+                List<Integer> parents2 = (List<Integer>) oldValuesparent.get(1);
+
+                enfants1.add(Integer.parseInt(data.getParam().second));
+                parents2.add(data.getParam().first);
+
+                ArrayList newValues1 = new ArrayList<>();
+                ArrayList newValues2 = new ArrayList<>();
+
+                newValues1.add(enfants1);
+                newValues1.add(parents1);
+
+                newValues2.add(enfants2);
+                newValues2.add(parents2);
+
+                dataset.put(data.getParam().first,newValues1);
+                dataset.put(Integer.valueOf(data.getParam().second),newValues2);
+
+            }
+        }
+
+        System.out.println(dataset.get(0));
+
 
 
     }
